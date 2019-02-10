f8844ddb377c68d0ca7e3bf9347fed1ef27bfe7b
Eric Van Der Dijs <eric2vanderdijs@gmail.com>
2019-02-09 23:32:54 -0400

    Add follows and followers fetching controllers for users

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,11 @@
 //local imports
 const { Users } = require('../models/users');
+const followersQ = require('../queries/followers');
+const { orderBy_toSQL } = require('../utils/general');
 
 module.exports.index = async (req, res, next) => {
   try {
-    let users = await Users.findAll();
+    let users = await Users.find();
     res.status(200).json( users.rows )
   } catch (error) {
     next(error);
@@ -50,4 +52,54 @@ module.exports.destroy = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
+}
+
+module.exports.userFollows = async (req, res, next) => {
+  try {
+    if( req.params.id === res.locals.payload.id ){
+      // get required data for db request
+      const GET_FOLLOWS_ID = followersQ.GET_FOLLOWS_ID({user_id: '$1'});
+      const orderBy = req.query.sort ? 
+        orderBy_toSQL(req.query.sort) : ['username DESC'];
+
+      let userFollows = 
+        await Users.find({
+          select: ['id', 'username', 'email'],
+          where: `id IN (${GET_FOLLOWS_ID})`,
+          orderBy: orderBy
+        }, [req.params.id]);
+
+        res.status(200).json({ follows: userFollows.rows });
+
+    } else {
+      throw new Error('USER_NOT_ALLOWED'); 
+    }
+  } catch (error) {
+    next(error); // pass the error to error handler
+  }
+}
+
+module.exports.userFollowers = async (req, res, next) => {
+  try {
+    if( req.params.id === res.locals.payload.id ){
+      // get required data for db request
+      const GET_FOLLOWERS_ID = followersQ.GET_FOLLOWERS_ID({follows: '$1'});
+      const orderBy = req.query.sort ? 
+        orderBy_toSQL(req.query.sort) : ['username DESC'];
+
+      let userFollowers = 
+        await Users.find({
+          select: ['id', 'username', 'email'],
+          where: `id IN (${GET_FOLLOWERS_ID})`,
+          orderBy: orderBy
+        }, [req.params.id]);
+
+        res.status(200).json({ followers: userFollowers.rows });
+        
+    } else {
+      throw new Error('USER_NOT_ALLOWED'); 
+    }
+  } catch (error) {
+    next(error); // pass the error to error handler
+  }z
 }
